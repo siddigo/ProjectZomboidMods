d973ffe47cfbefe06a816984a4bbb3ad548b634a
Sidnei <contador.sidnei@gmail.com>
2024-07-14 22:57:17 -0300

    feat(More Pictures): :sparkles: Add Pictures Procedural Distributions

---
+++ MorePictures/media/lua/server/Items/PicturesDistributions.lua
@@ -0,0 +1,8 @@
+require "Items/ProceduralDistributions"
+
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.NathanPic");
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 10);
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.DaniloPic");
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 10); 
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.EltonPic");
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 10);
