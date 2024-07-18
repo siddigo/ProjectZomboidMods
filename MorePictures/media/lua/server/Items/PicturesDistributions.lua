d619fa5372e75113c6b9a9d88e664cad1db5dd56
Sidnei <contador.sidnei@gmail.com>
2024-07-18 20:40:17 -0300

    feat(More Pictures): :sparkles: Added 2 more pictures

--- MorePictures/media/lua/server/Items/PicturesDistributions.lua
+++ MorePictures/media/lua/server/Items/PicturesDistributions.lua
@@ -1,8 +1,23 @@
 require "Items/ProceduralDistributions"
 
 table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.NathanPic");
-table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 10);
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.001);
 table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.DaniloPic");
-table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 10); 
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.001); 
 table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.EltonPic");
-table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 10);
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.001);
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.BrunoPic");
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.001);
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, "MorePictures.SidneiPic");
+table.insert(ProceduralDistributions.list["BedroomSideTable"].items, 0.001);
+
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, "MorePictures.NathanPic");
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, 0.001);
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, "MorePictures.DaniloPic");
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, 0.001); 
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, "MorePictures.EltonPic");
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, 0.001);
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, "MorePictures.BrunoPic");
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, 0.001);
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, "MorePictures.SidneiPic");
+table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, 0.001);
